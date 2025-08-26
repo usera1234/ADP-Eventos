e301cb8770676b2e4d65ae6246780bba7dd38f7b
Juan <chojuandelospalotes@gmail.com>
2025-08-25 21:45:00 -0300

    register button

--- components/app-banner.js
+++ components/app-banner.js
@@ -1,3 +1,5 @@
+import './app-button-register.js'
+
 class AppBanner extends HTMLElement {
     constructor(){
         super();
@@ -5,12 +7,19 @@ class AppBanner extends HTMLElement {
         this.innerHTML = `
             <div class="banner">
                 <div class="banner-item">
-                    <div class="banner-texto" style="color: black;">
+                    <div class="banner-texto" style="color: black;margin-left: 180px;">
                         EVENTOS LEGALTECH<br>COLEGIO DE ABOGADOS
                     </div>
                 </div>
                 <div class="banner-item" style="margin-top: 205px">
-                    <div class="banner-line" style="background-color:#2a8dbe; height: 200px; opacity: 0.4;"></div>
+                    <div class="banner-line" style="background-color:#2a8dbe; height: 200px; opacity: 0.4;">
+                        <div class="banner-item" style="flex-direction: row; margin-top: 50px; margin-left: 180px;">
+                            <div class="banner-texto" style="margin-left: 10px">
+                                Registrate aca:
+                            </div>
+                            <button class="btn" style="color: black;">Register</button>
+                        </div>
+                    </div>
                     <div class="banner-line"></div>
                 </div>
             </div>
---
+++ components/app-button-register.js
@@ -0,0 +1,14 @@
+class AppButtonRegister extends HTMLElement {
+    constructor(){
+        super();
+        
+        this.innerHTML = `
+            <div class="banner-texto" style="margin-left: 10px">
+                Registrate aca:
+            </div>
+            <button class="btn" onclick="window.location.href='https://entrawiz.app/events/feria-legaltech-2025-cpacf/register'"
+            style="color: black;"  >
+        `
+    }      
+}
+customElements.define('app-button-register', AppButtonRegister);
