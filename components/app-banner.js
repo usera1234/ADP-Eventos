e0700c04081b8f8fd41caf000a574c8be3c21b2b
Juan <chojuandelospalotes@gmail.com>
2025-08-28 20:26:24 -0300

    banner

--- components/app-banner.js
+++ components/app-banner.js
@@ -4,22 +4,17 @@ class AppBanner extends HTMLElement {
         
         this.innerHTML = `
             <div class="banner">
-                <div class="banner-item">
-                    <div class="banner-texto" style="color: black;">
-                        FERIA LEGALTECH<br>COLEGIO DE ABOGADOS
-                    </div>
-                </div>
+                <img src="static/images/fondo-banner.jpg" alt="banner" class="cronograma-imagen">               
                 <div class="banner-item" >
                     <div class="banner-line-register"">
                         <div class="banner-item" style="margin-top: 50px;">
-                            <div class="banner-texto" style="width: 1350px; text-decoration: none; margin-left: 190px; font-size: 60px">
+                            <div class="banner-texto" style="width: 1950px; text-decoration: none; margin-left: 190px; font-size: 60px">
                               <a href="https://entrawiz.app/events/feria-legaltech-2025-cpacf/register" target="_blank" style="text-decoration: none;">
                                 Registrate aca!
                               </a>
                             </div>
                         </div>
                     </div>
-                    <div class="banner-line"></div>
                 </div>
             </div>
         `;
